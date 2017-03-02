e2692d5188abde592c54a10587cc2dede20ad87e
Jun711 <cygoh@umich.edu>
2017-03-02 22:59:53 +0000

    Data Models and Internationalization

--- webApp/controller/App.controller.js
+++ webApp/controller/App.controller.js
@@ -6,8 +6,14 @@ sap.ui.define(
 	], function (Controller, MessageToast) {
 	
 		Controller.extend("opensap.myapp.controller.App", {
-			onShowHello: function() {
-				MessageToast.show("Hellow openSAP!");
-			}
+			onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		}
 		});	
 });
